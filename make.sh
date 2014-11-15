4d9b763e115f26b02c3d5d0e75e08454f2537537
Jan Fornoff <jan.fornoff@softwareag.com>
2014-11-15 10:08:09 +0100

    Initial commit, added vim directory and vimrc

---
+++ make.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+##############################################
+# Creates symlinks from dotfiles in ~/dotfiles
+# to their expected location
+##############################################
+
+### Vars
+
+dir=~/dotfiles
+olddir=~/dotfiles_old/
+files="vimrc vim"
+
+### Create backup folder for old dotfiles
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "Done!"
+
+### Change to dotfiles dir
+echo "Changing to $dir"
+cd $dir
+echo "Done!"
+
+# move existing dotfiles to olddir, then symlink from dotfiles directory
+for file in $files; do
+	echo "Moving any existing dotfiles from ~ to $olddir"
+	mv ~/.$file $olddir
+	echo "Symlinking!"
+	ln -s $dir/$file ~/.$file
+done
+echo "All done!"
