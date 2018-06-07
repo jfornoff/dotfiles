e0c118518a826298b508f60b9eb11c90bfd5ecd5
Jan Fornoff <jan.fornoff@grandcentrix.net>
2018-06-07 12:30:13 +0200

    Port to rcm

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if ! [ -x "$(command -v rcup)" ]; then
+  echo 'Error: rcm is not installed.' >&2
+  exit 1
+fi
+
+SYMLINK_DIRS="config conf.rc spacemacs.d secrets vim z" rcup
--- make_windows.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Copying vimrc"
-cp ~/dotfiles/vimrc ~/_vimrc
-
-echo "All done!"
