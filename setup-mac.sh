4ca7fb0acd76e82e0ba7bbc4a2f9fe20f4829dfa
Jan Fornoff <janfornoff@gmail.com>
2019-12-10 22:06:38 +0100

    Add Arq, tree. Add tap statements.

--- setup-mac.sh
+++ setup-mac.sh
@@ -4,10 +4,13 @@ echo "-- Writing system settings.."
 defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
 defaults write NSGlobalDomain KeyRepeat -int 2
 
+echo "-- Setting up brew taps.."
+brew tap thoughtbot/formulae
+brew tap homebrew/cask-fonts
 echo "-- Installing essential utilities.."
-ESSENTIAL_UTILS="rcm fzf neovim tmux zsh ripgrep"
+ESSENTIAL_UTILS="rcm fzf neovim tmux zsh ripgrep tree"
 echo "$ESSENTIAL_UTILS" | xargs brew install
 
 echo "-- Installing essential applications.."
-ESSENTIAL_APPS="alfred dash docker evernote flux font-fira-code iterm2 spectacle spotify"
+ESSENTIAL_APPS="alfred dash docker evernote flux font-fira-code iterm2 spectacle spotify arq"
 echo "$ESSENTIAL_APPS" | xargs brew cask install
