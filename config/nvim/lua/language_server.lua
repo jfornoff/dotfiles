b78e9c7e3abd5f3b21ad61bda6e6fbbaba345ca4
Jan Fornoff <janfornoff@gmail.com>
2023-01-23 19:47:03 +0100

    Invert down-up scrolling of LSP docs

--- config/nvim/lua/language_server.lua
+++ config/nvim/lua/language_server.lua
@@ -16,8 +16,8 @@ cmp.setup({
     -- documentation = cmp.config.window.bordered(),
   },
   mapping = cmp.mapping.preset.insert({
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-u>'] = cmp.mapping.scroll_docs(4),
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   }),
