107feb470f2489587e5b3902de78804dc6ad8779
Jan Fornoff <janfornoff@gmail.com>
2023-01-24 13:04:25 +0100

    Tab to complete suggestions

--- config/nvim/lua/language_server.lua
+++ config/nvim/lua/language_server.lua
@@ -19,7 +19,21 @@ cmp.setup({
     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
     ['<C-d>'] = cmp.mapping.scroll_docs(4),
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<C-n>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item(select_opts)
+      else
+        cmp.complete()
+      end
+    end, {'i', 's'}),
+    ['<C-p>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item(select_opts)
+      else
+        fallback()
+      end
+    end, {'i', 's'}),
+    ['<Tab>'] = cmp.mapping.confirm({select = true}),
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
