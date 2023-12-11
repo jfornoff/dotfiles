38f5890417032d7ad5cff69ee30e73915d7d64b1
Jan Fornoff <janfornoff@gmail.com>
2023-12-11 16:02:37 +0000

    Get rid of old cruft

--- setup-mac.sh
+++ setup-mac.sh
@@ -5,12 +5,11 @@ defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
 defaults write NSGlobalDomain KeyRepeat -int 2
 
 echo "-- Setting up brew taps.."
-brew tap thoughtbot/formulae
 brew tap homebrew/cask-fonts
 echo "-- Installing essential utilities.."
-ESSENTIAL_UTILS="asdf rcm fzf neovim tmux zsh ripgrep tree reattach-to-user-namespace fasd"
+ESSENTIAL_UTILS="asdf fzf neovim tmux zsh ripgrep tree reattach-to-user-namespace fasd"
 echo "$ESSENTIAL_UTILS" | xargs brew install
 
 echo "-- Installing essential applications.."
-ESSENTIAL_APPS="alfred bitwarden dash docker evernote flux font-fira-code iterm2 rectangle spotify arq"
+ESSENTIAL_APPS="alfred bitwarden dash docker notion flux font-fira-code iterm2 rectangle spotify arq"
 echo "$ESSENTIAL_APPS" | xargs brew install --cask
