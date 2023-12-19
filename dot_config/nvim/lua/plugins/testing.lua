05c5aecb6f05cd5b3f0c123d5abf4c346cbdb0cc
Jan Fornoff <janfornoff@gmail.com>
2023-12-19 16:43:03 +0000

    Use neovim vim-test strategy and exit terminal insert mode

--- dot_config/nvim/lua/plugins/testing.lua
+++ dot_config/nvim/lua/plugins/testing.lua
@@ -2,9 +2,12 @@
 return {
   'vim-test/vim-test',
   config = function()
+    vim.g['test#strategy'] = 'neovim'
+    -- Exit terminal insert mode easily
+    vim.keymap.set('t', '<C-o>', '<C-\\><C-n>')
+    -- Testing keymaps
     vim.keymap.set('n', '<leader>T', ':TestNearest<CR>')
     vim.keymap.set('n', '<leader>t', ':TestFile<CR>')
     vim.keymap.set('n', '<leader>a', ':TestSuite<CR>')
   end
 }
-
