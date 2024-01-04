ec1414eb91cb5d8923b324de1897393a69d32659
Jan Fornoff <janfornoff@gmail.com>
2024-01-04 13:41:19 +0000

    Start using trouble.nvim

---
+++ dot_config/nvim/lua/plugins/diagnostics.lua
@@ -0,0 +1,7 @@
+return {
+  "folke/trouble.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  keys = {
+    { "<leader>dt", "<cmd>TroubleToggle<cr>", desc = "Toggle diagnostics" }
+  },
+}
