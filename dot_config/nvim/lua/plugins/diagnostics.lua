7918a26c5452ef70475c15ee3510098ec23f887f
Jan Fornoff <janfornoff@gmail.com>
2025-09-25 11:47:01 +0000

    Fix trouble after breaking changes

--- dot_config/nvim/lua/plugins/diagnostics.lua
+++ dot_config/nvim/lua/plugins/diagnostics.lua
@@ -1,7 +1,9 @@
 return {
   "folke/trouble.nvim",
+  cmd = "Trouble",
+  opts = {},
   dependencies = { "nvim-tree/nvim-web-devicons" },
   keys = {
-    { "<leader>dt", "<cmd>TroubleToggle<cr>", desc = "Toggle diagnostics" }
+    { "<leader>dt", "<cmd>Trouble diagnostics toggle<cr>", desc = "Toggle diagnostics" }
   },
 }
