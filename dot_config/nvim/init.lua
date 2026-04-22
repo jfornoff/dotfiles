8e94b34443631fa048898f0499c69c0dd4f3252b
Jan Fornoff <janfornoff@gmail.com>
2026-04-22 22:09:41 +0200

    fix(nvim): remove Vim-isms, fix option abbreviations, set foldlevelstart
    Several settings in init.lua were either no-ops inherited from a Vim
    config or used terse abbreviations inconsistently with the rest of the
    file.
    
    Removed:
    - vim.opt.compatible = false — Neovim always runs in nocompatible mode;
      this line has no effect and only makes sense in a .vimrc
    - vim.opt.encoding = 'utf-8' — Neovim's internal encoding is always
      UTF-8 and cannot be changed; this is also a no-op
    
    Renamed for consistency with every other option in this file:
    - vim.opt.ts -> vim.opt.tabstop
    - vim.opt.vb -> vim.opt.visualbell
    
    Added:
    - vim.opt.foldlevelstart = 99 in the fold settings block. Without this,
      treesitter folds close all folds when a file is opened, making every
      file start fully collapsed. Setting it to 99 means files open fully
      expanded and folds are only closed when you explicitly invoke zc/zM.

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -25,11 +25,9 @@ require('lazy').setup({
 })
 
 -- General settings {{{
-vim.opt.encoding = 'utf-8'
-vim.opt.compatible = false
 vim.opt.autoindent = true
 vim.opt.fileformat = 'unix'
-vim.opt.ts = 2 -- indent by 2 spaces
+vim.opt.tabstop = 2 -- indent by 2 spaces
 vim.opt.shiftwidth = 2
 vim.opt.number = true
 vim.opt.ttimeoutlen = 100             -- 100ms to type more command characters
@@ -39,7 +37,7 @@ vim.opt.ignorecase = true             -- ignore case in search
 vim.opt.hlsearch = true               -- highlight all search matches
 vim.opt.smartcase = true              -- pay attention to case when caps are used
 vim.opt.incsearch = true              -- show search results as I type
-vim.opt.vb = true                     -- enable visual bell (disable audio bell)
+vim.opt.visualbell = true             -- enable visual bell (disable audio bell)
 vim.opt.scrolloff = 2                 -- minimum visible lines around cursor when scrolling
 vim.opt.list = true                   -- show tabs / trailing spaces
 vim.opt.listchars = 'tab:»·,trail:·'
@@ -49,9 +47,9 @@ vim.opt.modelines = 1                 -- Allow settings from vim comments in fil
 vim.cmd('runtime macros/matchit.vim') -- use % to jump between start/end of methods
 -- }}}
 
--- Fold settings {{{
-vim.opt.foldmethod = "expr"
+-- Fold settings {{{\nvim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldlevelstart = 99
 -- }}}
 
 require('mason').setup()
