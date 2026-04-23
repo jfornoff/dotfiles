54cc907b67dc8457d9570d6ec25de86339c3c75f
Jan Fornoff <janfornoff@gmail.com>
2026-04-23 08:43:32 +0200

    rebase onto current master

--- dot_config/nvim/lua/keybindings.lua
+++ dot_config/nvim/lua/keybindings.lua
@@ -1,3 +1,8 @@
+-- Closing windows quickly
 vim.keymap.set('n', '<leader>c', ':ccl<CR>')
+
+-- Remove search highlighting
 vim.keymap.set('n', '<C-l>', ':nohlsearch<CR>')
+
+-- Show diagnostics for current position
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
